a2f0d1d879d60df1df476a67c1e579d4a3fda93e
Edgar427 <Edgar.Htut@city.ac.uk>
2026-03-12 22:57:14 +0000

    basicex

---
+++ exercise1.py
@@ -0,0 +1,14 @@
+
+#learned new f string method
+
+item_name = "apples"
+quantity = 16
+price = 3.0
+in_stock = True
+
+message = f"Item name = {item_name}"
+
+print(message)
+print(f"Quantiyu = {quantity}")
+print(f"Price = {price}")
+print(f"In Stock? = {in_stock}")
